e295cd15146575f2a6bb2af4657bca08e4618016
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-04-24 01:48:03 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -6,11 +6,14 @@ from flask_migrate import Migrate
 
 from models import db, Earthquake
 
+# instance of Flask api
 app = Flask(__name__)
+# connection to SQLAlchemy ORM's database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# migration tool to apply SQLAlchemy ORM classes to sqlite database
 migrate = Migrate(app, db)
 db.init_app(app)
 
@@ -21,6 +24,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+# Add routes here
+@app.route('/earthquakes/<int:earthquake_id>')
+def earthquake_by_id(earthquake_id):
+    #if no earthquake exists, q will be None
+    q = Earthquake.query.filter_by(id=earthquake_id).first()
+    if q:
+        return make_response(q.to_dict())
+    else:
+        return make_response({
+            'message': f'Earthquake {earthquake_id} not found.'
+        }, 404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def matching_magnitude(magnitude):
+    q = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    q_list = [e.to_dict() for e in q]
+    #manually build up rest of JSON response
+    return make_response({
+        "count": len(q_list),
+        "quakes": q_list
+    })
 
 
 if __name__ == '__main__':
